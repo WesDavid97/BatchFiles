a7bf98c126b85d2f8b3be22794a6f24d1b358269
Wesley Silva <wdavidpontes@outlook.com>
2025-02-01 22:47:08 -0300

    Atualização MkLink

--- WindowsTerminal/MkLink (Adm).bat
+++ WindowsTerminal/MkLink (Adm).bat
@@ -2,29 +2,27 @@
 
 color A
 
-REM mesnagem para o usuário
 echo *Executar como Administrador*
 echo.
-echo Link Simbolico:
+echo Link Símbolico:
 echo O comando mklink no Windows e usado para criar links simbolicos, 
-echo links fisicos ou juncoes entre arquivos e pastas. Esses links permitem 
-echo que voce acesse um arquivo ou pasta em outro local como se fosse o original.
+echo links fisicos ou juncoes entre arquivos e pastas. Symlink ou Link 
+echo Símbolico permite manter a sincronização entre dois diretórios.
 echo.
-echo O Microsoft OneDrive pode impedir alteracoes pelo CMD em diretorios
-echo sincronizados, o link simbolico do arquivo pode ser criado na  Area
-echo de Trabalho e podera ser recortado para outro local. 
+echo O comando MkLink solicita primeiro o caminho de destino onde o link/atalho será criado,
+echo em seguida, o caminho de origem onde está o diretório que o usuário deseja cria o link.
 echo.
 
 REM Pergunta ao usuário pelo caminho do diretório
-set /p dirLink=">> Digite o caminho onde o Link sera criado: "
-set /p dirOrigem=">> Digite o caminho de origem a ser clonado: "
+set /p dirLink=">> Caminho de destino do Link : "
+set /p dirOrigem=">> Caminho de origem do diretorio: "
 echo.
 
 REM Obtém o nome do diretório a ser clonado 
 for %%f in ("%dirOrigem%") do set dirName=%%~nxf
 
 REM Cria o link simbólico na Área de Trabalho
-mklink /D "%dirLink%\%dirName%" "%dirOrigem%"
+mklink /D "%dirLink%\%SymlinkMkLink%\%dirName%" "%dirOrigem%"
 echo.
 
 pause
