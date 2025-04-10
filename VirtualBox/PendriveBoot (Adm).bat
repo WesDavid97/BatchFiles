6bbf43b7db5b54ffc8b119599ff5fbee069066df
Wesley David <150361792+WesDavid97@users.noreply.github.com>
2025-04-09 21:48:23 -0300

    Atualizar o PendriveBoot (Adm).bat

--- VirtualBox/PendriveBoot (Adm).bat
+++ VirtualBox/PendriveBoot (Adm).bat
@@ -1,35 +1,31 @@
 @echo off
-color A
+Color A
 
-REM Verifica se o VirtualBox está instalado ===
+REM Lista todos os discos com DISKPART ===
 echo.
-choice /M ">> VirtualBox esta instalado nesta maquina? "
-if errorlevel 2 (
-    echo.
-    echo [!] Instale o VirtualBox e execute novamente.
-    pause
-    exit /b
-) else (
-    REM Lista todos os discos com DISKPART ===
-    echo.
-    echo Listando discos locais e removiveis disponiveis...
-    echo.
-    (
-        echo list disk
-    ) | diskpart
-)
+echo Listando discos locais e removiveis disponiveis...
 echo.
-
+(
+    echo list disk
+) | diskpart
+echo.
 pause /b
 
 REM Solicita o número do disco (pendrive) ===
 choice /M ">> Criar disco virtual VMDK?"
 if errorlevel 2 (
     echo.
-    echo [!] Operação cancelada pelo usuário.
+    echo [] Operação cancelada pelo usuário.
     pause
     exit
 ) else (
+    echo [] O script cria um disco virtual (VMDK) para boot via usb para drivers
+    echo    removíveis bootavel.
+    echo [] Cada VMDK criado é unico, vinculado para cada drive.
+    echo [] Antes de iniciar o Virtual box precisa estar instalado.
+    echo [] Certificar se o drive usb, pendrive, HD ou SSD esta conectado.
+    echo [] Erros podem estar relacinados a VMDK não criada para novos drivers.
+    echo.
     set /p DISKNUM=">> Digite o numero do disco do pendrive:" 
     REM Cria o arquivo VMDK apontando para o pendrive
     echo Criando VMDK apontando para PhysicalDrive%DISKNUM%...
