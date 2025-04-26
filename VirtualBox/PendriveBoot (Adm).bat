6e4d2eaa772412fb85f02192bf3226c2b7fedb5e
Wesley Silva <wdavidpontes@outlook.com>
2025-04-26 01:46:15 -0300

    Atualização

--- VirtualBox/PendriveBoot (Adm).bat
+++ VirtualBox/PendriveBoot (Adm).bat
@@ -1,48 +1,30 @@
-@echo off 
+@echo off
 Color A
 
-REM Lista todos os discos com DISKPART ===
 echo.
-echo Listando discos locais e removiveis disponiveis...
+echo - O Virtual box precisa estar instalado antes de continuar.
+echo - O script cria um disco virtual (VMDK) para boot via usb para drivers removiveis bootavel.
+echo - Cada VMDK criado se torna unico, vinculado para cada drive.
+echo - Certificar se o drive removivel usb, pendrive, HD ou SSD esta conectado.
+echo - Erros podem estar relacinados a VMDK nao criada para novos drivers.
+echo.
+echo Abrindo o gerenciador de disco DISKPART...
 (
     echo list disk
-) | diskpart
+) | DISKPART 
 echo.
-pause /b
-
-REM Solicita o número do disco (pendrive) ===
 echo.
-echo [*] Antes de iniciar o Virtual box precisa estar instalado.
-choice /M ">> Criar disco virtual VMDK?"
-if errorlevel 2 (
-    echo.
-    echo [] Operação cancelada pelo usuário.
-    pause
-    exit
-) else (
-    echo [*] O script cria um disco virtual (VMDK) para boot via usb para drivers removíveis bootavel.
-    echo [*] Cada VMDK criado é unico, vinculado para cada drive.
-    echo [*] Certificar se o drive usb, pendrive, HD ou SSD esta conectado.
-    echo [*] Erros podem estar relacinados a VMDK não criada para novos drivers.
-    echo.
+set /p DISKNUM=">> Digite o numero do disco do pendrive:" 
+set /p FOLDERDISK=">> Salvar onde,caminho:"
+set VMPATCH=%FOLDERDISK%\VMBoxusb
+mkdir %VMPATCH%
+  
+echo Criando VMDK apontando para PhysicalDrive%DISKNUM%...
 
-    set /p DISKNUM=">> Digite o numero do disco do pendrive:" 
-    set /p FOLDERDISK=">>Salvar onde,caminho:"
-    set VMPATCH=%FOLDERDISK%\VMBoxusb\usb.vmdk
+REM Script para gerenciar o VirtualBox
+cd %programfiles%\Oracle\VirtualBox
+VBoxManage internalcommands createrawvmdk -filename %VMPATCH%\usb.vmdk -rawdisk \\.\PhysicalDrive%DISKNUM%
 
-    REM Cria o diretório para a pasta VMboxusb dentro do caminho especificado
-    if not exist "%VMPATCH%"(
-    mkdir VMPATCH
-    )
-    
-    echo Criando VMDK apontando para PhysicalDrive%DISKNUM%...
-
-    REM "cd" navega entre pastas ou diretórios o VboxManage é o comando para gerenciar o VirtualBox
-    cd %programfiles%\Oracle\VirtualBox
-    VBoxManage internalcommands createrawvmdk -filename %VMPATCH% -rawdisk \\.\PhysicalDrive%DISKNUM%
-
-    explorer /select , "%VMPATCH%"
-)
-
+explorer /select , "%VMPATCH%"
 pause
 exit
