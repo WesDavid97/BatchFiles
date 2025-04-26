41695cc53ff58ecd467c9eeefbf19da15ca05bb8
Wesley Silva <wdavidpontes@outlook.com>
2025-04-26 17:20:58 -0300

    Atualização

--- VirtualBox/PendriveBoot (Adm).bat
+++ VirtualBox/PendriveBoot (Adm).bat
@@ -2,29 +2,36 @@
 Color A
 
 echo.
-echo - O Virtual box precisa estar instalado antes de continuar.
-echo - O script cria um disco virtual (VMDK) para boot via usb para drivers removiveis bootavel.
-echo - Cada VMDK criado se torna unico, vinculado para cada drive.
-echo - Certificar se o drive removivel usb, pendrive, HD ou SSD esta conectado.
-echo - Erros podem estar relacinados a VMDK nao criada para novos drivers.
+echo PendriveBoot VirtualBox
+echo -------------------------------------------------------------------------------------------
+echo [*] Leia-me [*]
+echo [] O Virtual box precisa estar instalado antes de continuar.
+echo [] Certificar se o drive removivel usb, pendrive, HD ou SSD esta conectado.
+echo [] Este script precisar ser executado como administrador no mesmo repositorio 
+echo    do VirtualBox, por padrao o disco C:
+echo [] Cada VMDK criado se torna unico, vinculado para cada drive.
+echo [] Erros podem estar relacinados a VMDK nao criada para novos drivers.
+echo [] O script cria um disco virtual (VMDK) para boot via usb para drivers removiveis bootavel.
+echo -------------------------------------------------------------------------------------------
 echo.
 echo Abrindo o gerenciador de disco DISKPART...
 (
     echo list disk
-) | DISKPART 
+) | DISKPART
 echo.
 echo.
-set /p DISKNUM=">> Digite o numero do disco do pendrive:" 
-set /p FOLDERDISK=">> Salvar onde,caminho:"
-set VMPATCH=%FOLDERDISK%\VMBoxusb
-mkdir %VMPATCH%
-  
+
+set /p DISKNUM=">> Digite o numero do disco do removivel:" 
+set /p FOLDERDISK=">> Local onde salvar VirtualDisk Usb:"
+set VMPATH=%FOLDERDISK%\VirtualDiskUsb.vmdk
 echo Criando VMDK apontando para PhysicalDrive%DISKNUM%...
 
 REM Script para gerenciar o VirtualBox
-cd %programfiles%\Oracle\VirtualBox
-VBoxManage internalcommands createrawvmdk -filename %VMPATCH%\usb.vmdk -rawdisk \\.\PhysicalDrive%DISKNUM%
 
-explorer /select , "%VMPATCH%"
+cd "%programfiles%\Oracle\VirtualBox"
+VBoxManage createmedium disk --filename %VMPATH% --format=VMDK --variant RawDisk --property RawDrive=\\.\PhysicalDrive%DISKNUM%
+
+echo [*] VMDK criado com sucesso!
+explorer /select , "%FOLDERDISK%"
 pause
 exit
