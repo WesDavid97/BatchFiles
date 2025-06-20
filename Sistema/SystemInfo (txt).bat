e463c925e28e4a877afb6163954081ae49de7e78
Wesley Silva <wdavidpontes@outlook.com>
2025-06-20 13:14:03 -0300

    Atualização SystemInfo, alterado para comandos via poweshell para capturar informações de hardware. Acrescentado nome do host e log no nome do txt.

--- Sistema/SystemInfo (txt).bat
+++ Sistema/SystemInfo (txt).bat
@@ -1,46 +1,59 @@
 @echo off
-
 color A
 
-echo.
+echo Informacoes do PC
 echo Executando...
 
-	systeminfo >>SistemaInfo.txt
-	echo.
+REM Powershell Capturar data e hora formatada
+	for /f %%a in ('	powershell -Command "Get-Date -Format dd-MM-yyyy"') do set LogData=%%a
+
+REM Powershell C	apturar informações do computador (Fabricante, Modelo, Nome)
+	for /f %%b in ('	powershell -Command "(Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty Manufacturer)"') do set Manufacturer=%%b
+	set Manufacturer=%Manufacturer: =%
+	for /f %%c in ('	powershell -Command "(Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty Model)"') do set Model=%%c
+	set Model=%Model: =%
+	for /f %%d in ('	powershell -Command "(Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty Name)"') do set PCName=%%d
+
+REM Cria o nome do arquivo de log com base nas informações capturadas 
+	set LogFile=%Manufacturer%_%Model%_%LogData%.txt
+
+echo ==========================>>%LogFile%
+echo          SISTEMA >>%LogFile%
+echo ==========================>>%LogFile%
+	systeminfo >>%LogFile%
+	echo. >>%LogFile%
+
+echo ==========================>>%LogFile%
+echo          HARDWARE >>%LogFile%
+echo ==========================>>%LogFile%
+	echo.>>%LogFile%
 	
-	echo --------------------------------------------HARDWARE-------------------------------------------->>SistemaInfo.txt
-	echo.>>SistemaInfo.txt
+    echo == Placa Mae: >>%LogFile%
+		powershell -Command "Get-CimInstance Win32_BaseBoard | Select Manufacturer, Product, Version, SerialNumber">>%LogFile%
+	echo. >>%LogFile%
+
+	echo == BIOS: >>%LogFile%
+		powershell -Command "Get-CimInstance Win32_BIOS | Select SMBIOSBIOSVersion, SerialNumber">>%LogFile%
+	echo. >>%LogFile%
 	
-        echo Placa Mae: >>SistemaInfo.txt
-	wmic baseboard get product,Manufacturer,version,serialnumber >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
-	wmic bios get SMBIOSBIOSVersion >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
-	wmic bios get SerialNumber >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
+	echo == CPU: >>%LogFile%
+		powershell -Command "Get-CimInstance Win32_Processor | Select Name">>%LogFile%
+	echo. >>%LogFile%
 
-	echo Processador: >>SistemaInfo.txt
-	wmic cpu get name >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
+	echo == GPU: >>%LogFile%
+		powershell -Command "Get-CimInstance Win32_VideoController | Select Name, Caption, DeviceID, DriverDate, DriverVersion, Status">>%LogFile%
+	echo. >>%LogFile%
 
-	echo GPU: >>SistemaInfo.txt
-	wmic path win32_videocontroller get name, caption, deviceid, driverdate, driverversion, status >>SistemaInfo.txt
-        echo. >>SistemaInfo.txt
+	echo == RAM: >>%LogFile%
+		powershell -Command "Get-CimInstance Win32_PhysicalMemory | Select Capacity, Speed">>%LogFile%
+	echo. >>%LogFile%
 
-	echo Memoria RAM: >>SistemaInfo.txt
-	wmic memorychip get Capacity, Speed >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
+	echo == Armazenamento: >>%LogFile%
+		powershell -Command "Get-CimInstance Win32_DiskDrive | Select Model">>%LogFile%
+	echo. >>%LogFile%
 
-	echo Armazenamento: >>SistemaInfo.txt
-	wmic diskdrive get Model >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
-
-	echo Sistema Opercional: >>SistemaInfo.txt
-	wmic os get Caption,Version >>SistemaInfo.txt
-	echo. >>SistemaInfo.txt
-	
-	echo Exportado com sucesso, arquivo SistemaInfo.
-	echo.
+echo Exportado com sucesso, arquivo: %LogFile%
+echo.
 
 pause
 exite
